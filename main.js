dca822ed574d0e43134de0bd7fed946ad025ab37
Ichisada Shioko <ichisadashioko@gmail.com>
2020-10-07 16:05:45 +0700

    initialize webgl context

--- main.js
+++ main.js
@@ -3,3 +3,14 @@
  */
 let canvas = document.getElementById('canvas')
 console.log(canvas)
+
+let gl = canvas.getContext('webgl')
+
+if (gl === null) {
+    console.error('Unable to initialize WebGL. Your browser or machine may not support it.')
+} else {
+    // set clear color to black, fully opaque
+    gl.clearColor(0.0, 0.0, 0.0, 1.0)
+    // clear the color buffer with specified clear color
+    gl.clear(gl.COLOR_BUFFER_BIT)
+}
